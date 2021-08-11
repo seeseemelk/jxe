a5ee7778d5bd16fdb71027823fadc7b9adfa386d
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-08-11 22:40:03 +0200

    recompiler: add some documentation

--- src/main/java/be/seeseemelk/jtsc/recompiler/Recompiler.java
+++ src/main/java/be/seeseemelk/jtsc/recompiler/Recompiler.java
@@ -8,12 +8,24 @@ import java.nio.file.StandardOpenOption;
 
 import org.objectweb.asm.ClassReader;
 
+/**
+ * A class which can recompile entire programs from Java to D.
+ */
 public class Recompiler
 {
 	private final Path outputDirectory;
 	private final Path sourceDirectory;
 	private final Instrumentation instrumentation;
-	
+
+
+	/**
+	 * Creates a new recompiler.
+	 *
+	 * @param outputDirectory The directory in which converted files will be
+	 * stored.
+	 *
+	 * @throws IOException
+	 */
 	public Recompiler(Path outputDirectory, Instrumentation instrumentation) throws IOException
 	{
 		this.outputDirectory = outputDirectory;
@@ -23,18 +35,30 @@ public class Recompiler
 		Files.createDirectories(sourceDirectory);
 		createBuildScript("some_project");
 	}
-	
+
+	/**
+	 * Recompiles a file.
+	 *
+	 * @param input The file to convert.
+	 * @throws IOException
+	 */
 	public void recompile(InputStream input) throws IOException
 	{
 		var reader = new ClassReader(input);
-		
+
 		var imports = new ImportFinder();
 		reader.accept(imports, ClassReader.SKIP_DEBUG);
-		
+
 		var firstPass = new DClassVisitor(sourceDirectory, imports.getImports(), instrumentation);
 		reader.accept(firstPass, ClassReader.SKIP_DEBUG);
 	}
-	
+
+	/**
+	 * Generates a Dub build file.
+	 *
+	 * @param projectName The name of the project.
+	 * @throws IOException
+	 */
 	private void createBuildScript(String projectName) throws IOException
 	{
 		try (var writer = Files.newBufferedWriter(outputDirectory.resolve("dub.sdl"), StandardOpenOption.CREATE))
