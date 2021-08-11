ccee15e73c8f9732a14001b7ddcee4ff61f0185e
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-08-11 23:35:54 +0200

    tests: put instrumented sources in a different directory

--- tests/base.mk
+++ tests/base.mk
@@ -1,12 +1,13 @@
 JXE ?= java -jar ../../build/libs/jxe.jar
 SRC = $(shell find src -iname '*.java')
-CLASSES = $(SRC:src/%.java=bin/%.class)
+SUFFIX ?=
+CLASSES = $(SRC:src/%.java=bin$(SUFFIX)/%.class)
 
 .PHONY: test
 test: $(CLASSES)
-	$(JXE) $(JAVA_ARGS) bin conv
+	$(JXE) $(JAVA_ARGS) bin$(SUFFIX) conv$(SUFFIX)
 	cp tests.d conv/source
 	cd conv && dub test
 
-bin/%.class: src/%.java
-	javac -d bin $^
+bin$(SUFFIX)/%.class: src/%.java
+	javac -d bin$(SUFFIX) $^
