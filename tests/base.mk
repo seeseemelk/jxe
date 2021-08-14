ad66ae9ef59881a1af04b7b2721f2b5f89c86df5
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-08-14 22:41:34 +0200

    tests: build jxe when running test

--- tests/base.mk
+++ tests/base.mk
@@ -2,18 +2,25 @@ JXE ?= java -jar ../../build/libs/jxe-all.jar
 SRC = $(shell find src -iname '*.java')
 SUFFIX ?=
 CLASSES = $(SRC:src/%.java=bin$(SUFFIX)/%.class)
+BUILD_JXE ?= 1
 
-.PHONY: test
-test: $(CLASSES)
+.PHONY: test-normal
+test-normal: $(CLASSES) build-jxe
 	$(JXE) $(JAVA_ARGS) bin$(SUFFIX) conv$(SUFFIX)
 	cp tests.d conv$(SUFFIX)/source
 	cd conv$(SUFFIX) && dub test
 
 .PHONY: test-instrumented
-test-instrumented: $(CLASSES)
+test-instrumented: $(CLASSES) build-jxe
 	$(JXE) $(JAVA_ARGS) --only-instrumented bin$(SUFFIX) conv$(SUFFIX)
 	cp tests.d conv/source
 	cd conv && dub test
+
+.PHONY: build-jxe
+build-jxe:
+ifeq (1,$(BUILD_JXE))
+	cd ../.. && ./gradlew shadowJar
+endif
 
 bin$(SUFFIX)/%.class: src/%.java
 	javac -d bin$(SUFFIX) $^
