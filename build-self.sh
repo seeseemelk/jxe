d487d7b3323b9f8629e4372cc9212a825f42df7a
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-08-16 17:13:40 +0200

    Fix self hosted script a little

--- build-self.sh
+++ build-self.sh
@@ -3,15 +3,22 @@ set +e
 
 echo "============================"
 echo "Creating fresh build directories"
-rm -rf selfhosted
+rm -rf selfhosted extracted
 mkdir -p selfhosted/stage1
 
 echo "Building java JXE"
 ./gradlew shadowJar
 
+echo "Extracting shadow jar"
+mkdir extracted
+unzip build/libs/jxe-all.jar -d extracted
+
+echo "Removing META-INF"
+rm -rf extracted/META-INF
+
 echo "============================"
 echo "Building stage 1"
-java -jar ./build/libs/jxe-all.jar --only-instrumented src/main/java selfhosted/stage1
+java -jar ./build/libs/jxe-all.jar --only-instrumented extracted selfhosted/stage1
 cd selfhosted/stage1
 dub build
 cd ../..
@@ -19,7 +26,7 @@ cd ../..
 echo "============================"
 echo "Building stage 2"
 mkdir -p selfhosted/stage2
-java -jar ./selfhosted/stage1/jxe --only-instrumented src/main/java selfhosted/stage2
+java -jar ./selfhosted/stage1/jxe --only-instrumented extracted selfhosted/stage2
 cd selfhosted/stage2
 dub build
 cd ../..
@@ -27,7 +34,7 @@ cd ../..
 echo "============================"
 echo "Building stage 3"
 mkdir -p selfhosted/stage3
-java -jar ./selfhosted/stage2/jxe --only-instrumented src/main/java selfhosted/stage3
+java -jar ./selfhosted/stage2/jxe --only-instrumented extracted selfhosted/stage3
 cd selfhosted/stage3
 dub build
 cd ../..
