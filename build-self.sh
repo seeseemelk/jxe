aad9806f87782984da75266e6933b45f266e766d
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-08-16 17:07:46 +0200

    Add build script to test self hosted version

---
+++ build-self.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set +e
+
+echo "============================"
+echo "Creating fresh build directories"
+rm -rf selfhosted
+mkdir -p selfhosted/stage1
+
+echo "Building java JXE"
+./gradlew shadowJar
+
+echo "============================"
+echo "Building stage 1"
+java -jar ./build/libs/jxe-all.jar --only-instrumented src/main/java selfhosted/stage1
+cd selfhosted/stage1
+dub build
+cd ../..
+
+echo "============================"
+echo "Building stage 2"
+mkdir -p selfhosted/stage2
+java -jar ./selfhosted/stage1/jxe --only-instrumented src/main/java selfhosted/stage2
+cd selfhosted/stage2
+dub build
+cd ../..
+
+echo "============================"
+echo "Building stage 3"
+mkdir -p selfhosted/stage3
+java -jar ./selfhosted/stage2/jxe --only-instrumented src/main/java selfhosted/stage3
+cd selfhosted/stage3
+dub build
+cd ../..
+
+echo "============================"
+echo "Comparing stage 1 and stage 3"
+touch selfhosted/stage1/file
+diff -rq selfhosted/stage1 selfhosted/stage3 && echo "Success!" || (echo "Failed!" && false)
